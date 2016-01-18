8d14b174630df22137ecdd9681a6bc6183b0d945
dxtr85 <dxtr85@gmail.com>
2016-01-18 21:50:10 +0100

    First commit

---
+++ src/rd_sup.erl
@@ -0,0 +1,70 @@
+%%%-------------------------------------------------------------------
+%%% @author Jakub Mitoraj <>
+%%% @copyright (C) 2016, Jakub Mitoraj
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16 Jan 2016 by Jakub Mitoraj <>
+%%%-------------------------------------------------------------------
+-module(rd_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    RestartStrategy = one_for_one,
+    MaxRestarts = 0,
+    MaxSecondsBetweenRestarts = 1,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = permanent,
+    Shutdown = 2000,
+    Type = worker,
+
+    AChild = {resource_discovery, {resource_discovery, start_link, []},
+	      Restart, Shutdown, Type, [resource_discovery]},
+
+    {ok, {SupFlags, [AChild]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
